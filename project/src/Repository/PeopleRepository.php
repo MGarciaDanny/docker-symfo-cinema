db33759bbf04a736911b7c7d45ffce0a763f3fcc
dgarcia <m.garcia.danny@gmail.com>
2023-06-05 00:18:16 +0200

    findbyId

--- project/src/Repository/PeopleRepository.php
+++ project/src/Repository/PeopleRepository.php
@@ -39,28 +39,27 @@ class PeopleRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return People[] Returns an array of People objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return People[] Returns an array of People objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?People
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findById($value): ?People
+    {
+        return $this->createQueryBuilder('People')
+            ->andWhere('People.id = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
