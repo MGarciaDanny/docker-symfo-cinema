2b20355b429fa4666a2a2b815a2e6ba7b7960c51
dgarcia <m.garcia.danny@gmail.com>
2023-06-08 19:38:42 +0200

    crud movie entity

--- project/src/Controller/PublicController.php
+++ project/src/Controller/PublicController.php
@@ -16,7 +16,7 @@ class PublicController extends AbstractController
         ]);
     }
 
-    #[Route('/movie', name: 'app.get.movie')]
+    #[Route('/test-movie', name: 'app.get.movie')]
     public function movie(): Response
     {
         return $this->json(['movie'=>'todo']);
